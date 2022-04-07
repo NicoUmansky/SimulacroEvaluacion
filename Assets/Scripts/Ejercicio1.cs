da906ad02bd0853294e3d5294390e4c175b40055
NicoUmansky <umanskynico@gmail.com>
2022-04-07 13:50:05 -0300

    13:50

---
+++ Assets/Scripts/Ejercicio1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio1 : MonoBehaviour
+{
+    // 1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda 
+    //si son iguales o no lo son.
+    public int num1, num2;
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Son numeros iguales");
+        }
+        else
+        {
+            Debug.Log("No son iguales");
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/Ejercicio2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2 : MonoBehaviour
+{
+    // 2. Crear las variables necesarias para ingresar por Inspector el precio de 3 productos y
+    //un monto de dinero disponible. Comunicar si la suma de los mismos supera o no el monto de dinero disponible. Luego indicar cuánto dinero sobra o falta.
+    public float precio1, precio2, precio3, montodispo;
+    void Start()
+    {
+        if (precio1 + precio2 + precio3 > montodispo)
+        {
+            Debug.Log("La suma de los productos supera el monto de dinero disponible");
+            Debug.Log("Falta agregar $" + (precio1 + precio2 + precio3 - montodispo));
+        }
+        else if (precio1 + precio2 + precio3 <montodispo) {
+            Debug.Log("La suma de los productos no supera el monto de dinero disponible");
+            Debug.Log("Sobran $" + (montodispo - (precio1 + precio2 + precio3)));
+        }
+
+    }
+
+    
+}
